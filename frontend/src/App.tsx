dfa5f3df5f2d8cd2bb67b52163fea4bdc84c9209
Leo Febba <leonardofebba@hotmail.com>
2022-07-14 20:54:57 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
 import { useState } from "react"
+import NotificatoinButton from "./components/NotificationButton"
 
 function App() {
     return (
+      <>
       <h1>Olá mundo</h1>
+      <NotificatoinButton />
+      <NotificatoinButton />
+      </>
     )
 }
 
