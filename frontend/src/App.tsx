1f462cf27947bf1ac5b018d5ab23f65aa58c603a
Leo Febba <leonardofebba@hotmail.com>
2022-07-14 21:22:16 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,21 @@
-import { useState } from "react"
-import NotificatoinButton from "./components/NotificationButton"
+import { useState } from "react";
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
 
 function App() {
-    return (
-      <>
-      <h1>Olá mundo</h1>
-      <NotificatoinButton />
-      <NotificatoinButton />
-      </>
-    )
+  return (
+    <>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+          <div className="dsmeta-card"></div>
+        </section>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
